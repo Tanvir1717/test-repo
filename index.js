75d5c874f5c2425679897b8ef51f1bd5cc37a0e6
Tanvir Ahmed <tanvirahmed420@outlook.com>
2021-12-28 16:51:54 +0600

    Initialize Module

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 //2- npm init
 //3- simply enter and get json file
 //4- declare as git repository= git init
+//5- git add .,git commit and check log
+//6- We will install package= npm install bootstrap --save
+//7- see package-lock.json file and check package.json file bootstrap showing
+//8- we will ignore node module file cause unnecessary, create .gitignore file
+//9- add /node-modules to .gitignore and check git status
+//10- git ads . , git commit -m "Initialize module"
